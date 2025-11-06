2cdc38dbaf751f16d6231cc0e8edc91f79c6e6c9
Lakhan-bit <lakhankhobragade2003@gmail.com>
2025-11-06 10:12:52 +0530

    first changes

---
+++ tests/test-3.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://ipoplatform.com/');
+  // await page.getByRole('button', { name: 'Anchor Investors' }).click();
+  // await page.getByRole('link', { name: 'SME IPO Anchor Investors List' }).click();
+  // await page.getByRole('link', { name: 'Rajasthan Global Securities' }).click();
+  // await page.getByRole('link', { name: 'Total No. of SME IPOs' }).click();
+  // await page.getByRole('link', { name: 'Next' }).click();
+  // await page.getByRole('link', { name: '13' }).click();
+  // await page.getByRole('navigation', { name: 'pagination' }).getByLabel('Next').click();
+  // await page.getByRole('navigation', { name: 'pagination' }).getByLabel('Next').click();
+});
